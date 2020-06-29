472894536b2817154ac5c1abb0e013aa312867d9
Ricky Lei <Rickylei7@gmail.com>
2020-06-28 20:27:57 -0600

    set new meta

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,16 +1,16 @@
 <template>
   <nav>
-    <router-link to="/money" class="item">
+    <router-link to="/money" class="item" active-class="selected">
       <Icon name="money"/>
       Money
     </router-link>
 
-    <router-link to="/labels" class="item">
+    <router-link to="/labels" class="item" active-class="selected">
       <Icon name="labels"/>
       Labels
     </router-link>
 
-    <router-link to="/statistics" class="item">
+    <router-link to="/statistics" class="item" active-class="selected">
       <Icon name="statistics"/>
       Statistics
     </router-link>
@@ -43,6 +43,9 @@
         width: 32px;
         height: 32px;
       }
+    }
+    > .item.selected{
+      color: red;
     }
   }
 </style>
