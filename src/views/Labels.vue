8b9fe8686f3984b025f5b76bc237b2bb670c8385
Ricky Lei <Rickylei7@gmail.com>
2020-07-04 15:08:47 -0600

    Labels.vue CSS done

--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -1,10 +1,27 @@
 <template>
-  <div>
+  <Layout>
+    <ol class="tags">
+      <li>
+        <span>food</span>
+        <Icon name="right"/>
+      </li>
+      <li>
+        <span>house</span>
+        <Icon name="right"/>
+      </li>
+      <li>
+        <span>transport</span>
+        <Icon name="right"/>
+      </li>
+    </ol>
 
-   <Layout>
-     <p>Labels111</p>
-   </Layout>
-  </div>
+    <div class="createTag-wrapper">
+      <button class="createTag">
+        New Tag
+      </button>
+    </div>
+  </Layout>
+
 </template>
 
 <script lang="ts">
@@ -13,8 +30,38 @@
   };
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+  .tags {
+    background: white;
+    font-size: 16px;
+    padding-left: 16px;
+    > li {
+      min-height: 44px;
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      border-bottom: 1px solid #e6e6e6;
+      svg {
+        width: 240px;
+        height: 24px;
+        color: #666;
+        margin-right: 16px;
+      }
+    }
+  }
+  .createTag {
+    background: #767676;
+    color: white;
+    border-radius: 4px;
+    border: none;
+    height: 40px;
+    padding: 0 16px;
+    &-wrapper {
+      text-align: center;
+      padding: 16px;
+      margin-top: 44-16px;
+    }
+  }
 
 </style>
 
